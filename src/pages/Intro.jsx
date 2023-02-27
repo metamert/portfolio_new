d642ace4ed06156935dc826a6fe2ae582ba33e77
orkun <orkunmertgs41@gmail.com>
2023-02-27 19:48:21 +0100

    update

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -95,7 +95,7 @@ const [timer,setTimer]=React.useState(false)
               <br />
               Retired partner from Deloitte.
                   <br />
-   Hi friends, if you need a hard-working, coachable and friendly freelance React/web/app developer then please get in touch with Orkun. He's been instrumental in the progress we've made on Nooli in recent months, willing to put in the effort and long hours required for startups and always with a smile.
+   'Hi friends, if you need a hard-working, coachable and friendly freelance React/web/app developer then please get in touch with Orkun. He's been instrumental in the progress we've made on Nooli in recent months, willing to put in the effort and long hours required for startups and always with a smile.'
             </p>
         </h1>
         </div>
